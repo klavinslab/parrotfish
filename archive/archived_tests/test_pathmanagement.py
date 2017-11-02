028bc7c06b64b951f896cdd0ed8b9c9ac351f21a
jvrana <justin.vrana@gmail.com>
2017-11-01 21:14:40 -0700

    removed archived code

--- archive/archived_tests/test_pathmanagement.py
+++
@@ -1,6 +0,0 @@
-from parrotfish import *
-import pytest
-
-def test_paths():
-
-    ParrotFish.category = "Justin"
--- archive/reconnect.py
+++
@@ -1,5 +0,0 @@
-# TODO: this file reconnects a root folder to pfish
-# could reconstruct env.pkl
-# TODO: could copy environment pickle in root folder?
-# TODO: why can't env.pkl be in the root folder? (you may not want your credentials exposed...)
-# TODO: try to relogin into aquarium session if it doesn't work
